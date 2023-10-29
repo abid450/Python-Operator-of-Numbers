f76dcb896155611984dbfced217131365611c883
Abid Hasan <144882936+abid450@users.noreply.github.com>
2023-10-29 10:35:05 +0600

    I have a upload python file

---
+++ main.py
@@ -0,0 +1,130 @@
+
+# Ternary Operator ------------------
+ 
+"""num1 = 20
+num2 = 10
+if num1 > num2:
+      print(num1)
+
+else:
+      print(num2)      
+"""
+
+# Logical Operator -------------------------
+"""
+num1 = 120
+num2 = 50
+num3 = 90
+
+if num1 > num2 and num1 > num3:
+          print(num1)
+elif num2 > num1 and num2 > num3:
+          print(num2)
+
+else:
+        print(num3)          """
+
+
+
+# Vowel Consonant for Logical Operator ------------------
+
+""" ch = "s"
+
+if ch == "a" or ch == "e" or ch == "i" or ch == "o" or ch == "u":
+   print("Vowel")
+
+else:
+    print("consonant") """
+
+
+# Use by or , and ---------------------------------------
+
+""" marks = 100
+
+if marks >= 80 and marks <=100:
+           print("A+")
+elif marks >= 70 and marks <=79:
+        print("A")
+
+elif marks >= 60 and marks <=69:
+        print("A-")
+
+elif marks >= 50 and marks <=59:
+        print("C")
+
+elif marks >= 40 and marks <=49:
+        print("D") """
+
+
+# While , Loop Usage --------------------------------------
+"""
+i = 1
+
+while i<=20:
+    print("Bangladesh")
+    i = i + 1
+
+print("program end")   """
+
+
+
+"""
+sum = 0
+i = 1
+while i <= 100:
+     sum = sum + i
+     i = i + 1
+
+     print(sum)
+"""
+
+# Sum Of Number ------------------------------ 
+
+"""n = int(input("Enter the Number : "))
+sum = 0
+i = 1
+while i <= n:
+      sum = sum + i
+      i = i + 1
+
+print (sum)"""
+
+
+# list --------------------------------------
+
+"""subjects = ["C","C++","Java","python","Javascript","php"]
+
+print(subjects + ["html"] + ["css"])
+"""
+
+# Remove , Clear --------------------
+
+"""subjects = ["C","C++","Java","python","Javascript","php"]
+subjects.clear()
+print(subjects)
+"""
+
+# Sort , revarse ----------------------
+
+""" subjects = ["C","C++", "Basic", "Java","python","Javascript","php"]
+subjects.sort()
+print(subjects)
+
+
+subjects = ["C","C++","Java","python","Javascript","php"]
+subjects.reverse()
+print(subjects)
+                """
+
+subjects = [20,106,200,500,302,221,40,50]
+subjects.sort()
+print(subjects)
+
+# Copy ----------------------
+
+subjects2 = subjects.copy()
+print(subjects2)
+
+
+
+
